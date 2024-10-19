0bd3e3a86626c0cc5a6fa6a0032a350abd68b4c0
Tab Ye <tabye320@outlook.com>
2024-10-19 22:39:34 +0800

    Base64 structure

---
+++ xmake.lua
@@ -0,0 +1,20 @@
+add_languages("cxx20")
+
+target("tbase64")
+    set_kind("headeronly")
+    add_headerfiles("include/TBase64.hpp")
+target_end()
+
+target("MainTest")
+    set_kind("binary")
+    add_files("src/main.cpp")
+    add_deps("tbase64")
+    add_includedirs("include")
+target_end()
+
+target("BenchMark")
+    set_kind("binary")
+    add_files("src/benchmark.cpp")
+    add_deps("tbase64")
+    add_includedirs("include")
+target_end()
